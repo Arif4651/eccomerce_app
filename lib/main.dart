39370cae4b5c80f67fd2b8616d44acc19b832211
Arif4651 <arif465109@gmail.com>
2025-05-21 11:18:47 +0600

    initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/homepage.dart';
+
+void main()
+{
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+
+
+    );
+  }
+}
